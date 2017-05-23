5056318d5d70a8c09571930a56fea8ae4ebf8ab3
louguozhe <louguozhe@163.com>
2017-05-23 11:35:43 +0800

    增加 spider fenlei

--- hudongbaike/hudongbaike/spiders/fenlei.py
+++ hudongbaike/hudongbaike/spiders/fenlei.py
@@ -4,8 +4,15 @@ import scrapy
 
 class FenleiSpider(scrapy.Spider):
     name = 'fenlei'
-    allowed_domains = ['http://fenlei.baike.com/']
-    start_urls = ['http://http://fenlei.baike.com//']
+    allowed_domains = ['fenlei.baike.com']
+    start_urls = ['http://fenlei.baike.com/']
 
     def parse(self, response):
-        pass
+        for dd in response.css('dd'):
+            yield {'dd': dd.css('a ::text').extract_first()}
+        #for dd in response.xpath('//*[@id="f-a"]/div/div/dl/dd'):
+        #    yield {'dd': dd.css('a ::text').extract_first()}
+
+        #next_page = response.css('div.prev-post > a ::attr(href)').extract_first()
+        #if next_page:
+        #    yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
